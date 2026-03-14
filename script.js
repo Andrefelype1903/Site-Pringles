4c0a53fb02a77d884e571bd61d064060e8728d3e
andrefelype <andrefilype21122014@gmail.com>
2026-03-13 21:01:08 -0300

    add gsap

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+gsap.registerPlugin(DrawSVGPlugin, ScrollTrigger, ScrollSmoother, SplitText);
+
 const lata1 = document.querySelector(".lata1");
 const lata2 = document.querySelector(".lata2");
 const lata3 = document.querySelector(".lata3");
@@ -8,6 +10,11 @@ const lata8 = document.querySelector(".lata8");
 const lata9 = document.querySelector(".lata9");
 const lata10 = document.querySelector(".lata10");
 const lata5 = document.querySelector(".lata5");
+
+const body = document.querySelector("body");
+const spans = document.querySelector(".spans");
+
+console.log(spans);
 
 const saltEVinagar = document.querySelector(" .slider .saltEVinagar");
 const hamECheese = document.querySelector(".hamECheese");
@@ -22,16 +29,43 @@ const original = document.querySelector(".original");
 
 const slides = document.querySelectorAll(".slide");
 
+const criaSpan = () => {
+  slides.forEach(() => {
+    const span = document.createElement("span");
+    span.classList.add("span");
+    spans.appendChild(span);
+  });
+};
+
+criaSpan();
+
 let zIndex = 1;
 
 const ativarSlide = (slide) => {
   zIndex++;
+
   slide.classList.add("active");
   slide.style.zIndex = zIndex;
+
+  if (slide.classList.contains("emmental")) {
+    body.style.color = "#db8205";
+  } else {
+    body.style.color = "rgba(255, 255, 255, 0.9)";
+  }
 };
 
 lata1.addEventListener("click", () => {
   ativarSlide(saltEVinagar);
+
+  gsap.to(".original .saborBatata", {
+    rotation: 130,
+    duration: 1,
+  });
+
+  gsap.to(".saltEVinagar .saborBatata", {
+    rotation: -30,
+    duration: 1,
+  });
 
   setTimeout(() => {
     original.classList.remove("active");
@@ -40,6 +74,17 @@ lata1.addEventListener("click", () => {
 
 lata2.addEventListener("click", () => {
   ativarSlide(hamECheese);
+
+  gsap.to(".saltEVinagar .saborBatata", {
+    rotation: 130,
+    duration: 1,
+  });
+
+  gsap.to(".hamECheese .saborBatata", {
+    rotation: -30,
+    duration: 1,
+  });
+
   setTimeout(() => {
     saltEVinagar.classList.remove("active");
   }, 1000);
@@ -47,6 +92,17 @@ lata2.addEventListener("click", () => {
 
 lata3.addEventListener("click", () => {
   ativarSlide(paprika);
+
+  gsap.to(".hamECheese .saborBatata", {
+    rotation: 130,
+    duration: 1,
+  });
+
+  gsap.to(".paprika .saborBatata", {
+    rotation: -30,
+    duration: 1,
+  });
+
   setTimeout(() => {
     hamECheese.classList.remove("active");
   }, 1000);
@@ -54,6 +110,17 @@ lata3.addEventListener("click", () => {
 
 lata4.addEventListener("click", () => {
   ativarSlide(cheeseEOnion);
+
+  gsap.to(".paprika .saborBatata", {
+    rotation: 130,
+    duration: 1,
+  });
+
+  gsap.to(".cheeseEOnion .saborBatata", {
+    rotation: -30,
+    duration: 1,
+  });
+
   setTimeout(() => {
     paprika.classList.remove("active");
   }, 1000);
@@ -61,6 +128,17 @@ lata4.addEventListener("click", () => {
 
 lata6.addEventListener("click", () => {
   ativarSlide(hotESpicy);
+
+  gsap.to(".cheeseEOnion .saborBatata", {
+    rotation: 130,
+    duration: 1,
+  });
+
+  gsap.to(".hotESpicy .saborBatata", {
+    rotation: -30,
+    duration: 1,
+  });
+
   setTimeout(() => {
     cheeseEOnion.classList.remove("active");
   }, 1000);
@@ -68,6 +146,17 @@ lata6.addEventListener("click", () => {
 
 lata7.addEventListener("click", () => {
   ativarSlide(saborJamon);
+
+  gsap.to(".hotESpicy .saborBatata", {
+    rotation: 130,
+    duration: 1,
+  });
+
+  gsap.to(".saborJamon .saborBatata", {
+    rotation: -30,
+    duration: 1,
+  });
+
   setTimeout(() => {
     hotESpicy.classList.remove("active");
   }, 1000);
@@ -75,6 +164,17 @@ lata7.addEventListener("click", () => {
 
 lata8.addEventListener("click", () => {
   ativarSlide(emmental);
+
+  gsap.to(".saborJamon .saborBatata", {
+    rotation: 130,
+    duration: 1,
+  });
+
+  gsap.to(".emmental .saborBatata", {
+    rotation: -30,
+    duration: 1,
+  });
+
   setTimeout(() => {
     saborJamon.classList.remove("active");
   }, 1000);
@@ -82,6 +182,17 @@ lata8.addEventListener("click", () => {
 
 lata9.addEventListener("click", () => {
   ativarSlide(sourCream);
+
+  gsap.to(".emmental .saborBatata", {
+    rotation: 130,
+    duration: 1,
+  });
+
+  gsap.to(".sourCream .saborBatata", {
+    rotation: -30,
+    duration: 1,
+  });
+
   setTimeout(() => {
     emmental.classList.remove("active");
   }, 1000);
@@ -89,6 +200,17 @@ lata9.addEventListener("click", () => {
 
 lata10.addEventListener("click", () => {
   ativarSlide(bbqTexas);
+
+  gsap.to(".sourCream .saborBatata", {
+    rotation: 130,
+    duration: 1,
+  });
+
+  gsap.to(".bbqTexas .saborBatata", {
+    rotation: -30,
+    duration: 1,
+  });
+
   setTimeout(() => {
     sourCream.classList.remove("active");
   }, 1000);
@@ -96,6 +218,17 @@ lata10.addEventListener("click", () => {
 
 lata5.addEventListener("click", () => {
   ativarSlide(original);
+
+  gsap.to(".bbqTexas .saborBatata", {
+    rotation: 130,
+    duration: 1,
+  });
+
+  gsap.to(".original .saborBatata", {
+    rotation: -30,
+    duration: 1,
+  });
+
   setTimeout(() => {
     bbqTexas.classList.remove("active");
   }, 1000);
